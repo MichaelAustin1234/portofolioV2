c8c28c89b887f0926f1806c58b7eb4f43fe0b1cc
MichaelAustin1234 <MichaelAustin1234@users.noreply.github.com>
2026-08-04 11:57:04 +0700

    style(hero): make all roles uniform in terracotta color

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,8 +24,8 @@ export default function Hero() {
             
             {/* Multi-Role Primary Headline */}
             <div>
-              <h1 className="text-xl sm:text-2xl lg:text-3xl font-display font-extrabold text-[#121212] tracking-tight leading-snug">
-                <span className="text-[#B94A24]">System Analyst</span> <span className="text-[#66625D]/30 mx-1.5">•</span> Web Developer <span className="text-[#66625D]/30 mx-1.5">•</span> Software Engineer <span className="text-[#66625D]/30 mx-1.5">•</span> IT Support <span className="text-[#66625D]/30 mx-1.5">•</span> QA
+              <h1 className="text-xl sm:text-2xl lg:text-3xl font-display font-extrabold text-[#B94A24] tracking-tight leading-snug">
+                System Analyst <span className="text-[#66625D]/40 mx-1.5">•</span> Web Developer <span className="text-[#66625D]/40 mx-1.5">•</span> Software Engineer <span className="text-[#66625D]/40 mx-1.5">•</span> IT Support <span className="text-[#66625D]/40 mx-1.5">•</span> QA
               </h1>
             </div>
 
